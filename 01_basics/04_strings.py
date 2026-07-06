7f05475698c8b370b2500b5abc2a2e390266521f
Grzegorz <gpoznanski98@gmail.com>
2026-07-06 22:06:36 +0200

    feat: add strings basics exercises

---
+++ 01_basics/04_strings.py
@@ -0,0 +1,11 @@
+phrase = "Grzegorz Poznanski"
+print(phrase + " has 28 years old.")
+print(phrase.lower())
+# Very helpfull use 2 or more function in one variables
+print(phrase.upper().isupper())
+# printed out first symbol in string - phrase
+print(phrase[0])
+# show which number of index have symbol in string , very usefull. Where ''gorz'' is strating.
+print(phrase.index("gorz"))
+# replace function
+print(phrase.replace("Poznanski", "Nowak"))
---
+++ exercises/01_basics/string_profile.py
@@ -0,0 +1,10 @@
+first_name = "Grzegorz"
+career_goal = "Junior Data Engineer"
+main_language = "Python"
+cloud = "AWS"
+
+print(first_name.upper())
+print(first_name.lower())
+print(len(career_goal))
+print(main_language[0])
+print(cloud.replace("AWS", "Amazon Web Services"))
