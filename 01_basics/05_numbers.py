9e6601f263e23bab088fcef50c3112ef1862b474
Grzegorz <gpoznanski98@gmail.com>
2026-07-06 23:14:57 +0200

    feat: add numbers basics exercises

--- 01_basics/05_numbers.py
+++ 01_basics/05_numbers.py
@@ -3,9 +3,13 @@ print(2.5)
 print(3 + 4)
 print(3 * 4)
 print(10 / 2)
+# Reszta z dzielenia przez 3 a wiec wynik to 1
 print(10 % 3)
+# Wartosc bezwzgledna z 5
 print(abs(-5))
+# potegi
 print(pow(2, 3))
 print(max(4, 10))
 print(min(4, 10))
+# zaokraglenie liczby to gory w tym wypadku
 print(round(3.7))
