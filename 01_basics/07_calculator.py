867782087aa0305e758875a40c9712765e000518
Grzegorz <gpoznanski98@gmail.com>
2026-07-08 23:30:07 +0200

    feat: add input and calculator exercises

---
+++ 01_basics/07_calculator.py
@@ -0,0 +1,5 @@
+# Calculator
+num1 = input("Enter first number : ")
+num2 = input("Enter second number : ")
+result = float(num1) + float(num2)
+print("Addition result is : " + str(result))
---
+++ exercises/01_basics/basic_calculator.py
@@ -0,0 +1,10 @@
+first_number = input("Enter first number: ")
+second_number = input("Enter second number: ")
+
+first_number = float(first_number)
+second_number = float(second_number)
+
+addition_result = first_number + second_number
+subtraction_result = first_number - second_number
+multiplication_result = first_number * second_number
+division_result = first_number / second_number
