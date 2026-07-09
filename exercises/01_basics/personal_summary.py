fa05fe297f7287fe592c42865b107ed461c76461
Grzegorz <gpoznanski98@gmail.com>
2026-07-09 23:00:07 +0200

    feat: add input and calculator exercises

---
+++ exercises/01_basics/personal_summary.py
@@ -0,0 +1,15 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+goal = input("Enter your career goal: ")
+time = input("Enter your weekly learing hours: ")
+
+print("====Profile=====")
+print("Your name is " + name + "\n" + "Your Age is: " + age)
+print(
+    "Your career goal is: "
+    + goal
+    + "\n"
+    + "You have: "
+    + time
+    + " hours learining per week"
+)
