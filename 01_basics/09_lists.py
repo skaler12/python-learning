76188eb204125ed517ac133d0199f0c3888b2672
Grzegorz <gpoznanski98@gmail.com>
2026-07-11 23:30:00 +0200

    feat: learning lists

---
+++ 01_basics/09_lists.py
@@ -0,0 +1,13 @@
+friends = ["Kevin", "Karen", "Jim", "Oscar", "Toby"]
+
+# wypisz cala liste
+print(friends)
+
+# 1 element w liscie
+print(friends[0])
+
+# ostatni element w liscie
+print(friends[-1])
+
+# Element 2 i 3 bez elementu 4 w liscie
+print(friends[1:3])
---
+++ exercises/01_basics/learning_topics.py
@@ -0,0 +1,13 @@
+topics = ["Python", "SQL", "ETL", "AWS", "Docker", "Airflow"]
+
+print("All topics:")
+print(topics)
+
+print("First topic:")
+print(topics[0])
+
+print("Last topic:")
+print(topics[-1])
+
+print("First three topics:")
+print(topics[0:3])
