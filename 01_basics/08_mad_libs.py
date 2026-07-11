bf1e5981344053bc3c22331e37cf6de72544645a
Grzegorz <gpoznanski98@gmail.com>
2026-07-11 22:56:25 +0200

    feat: add mad libs game

---
+++ 01_basics/08_mad_libs.py
@@ -0,0 +1,7 @@
+animal = input("Enter animal: ")
+colour = input("Enter colour: ")
+age = input("Enter age: ")
+
+print(animal + " is the best")
+print(colour + " is my favourite")
+print(age + " is age ")
