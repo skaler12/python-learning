18ba5b4e5d02828f70fb80afaa53c4ef38ba4720
Grzegorz <gpoznanski98@gmail.com>
2026-07-12 23:35:03 +0200

    if statement

---
+++ exercises/01_basics/14_learning_decisions.py
@@ -0,0 +1,10 @@
+weekly_hours = 12
+uses_git = True
+finishes_basics = False
+
+if weekly_hours >= 10 and uses_git:
+    print("You have a solid learning setup.")
+if finishes_basics:
+    print("You can start your first mini project.")
+else:
+    print("Keep practicing Python basics.")
