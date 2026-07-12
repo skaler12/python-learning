c1673d74f0baec7c0f2c605a789d780cf97f4cfd
Grzegorz <gpoznanski98@gmail.com>
2026-07-12 14:44:09 +0200

    Tuples larning

---
+++ 01_basics/11_tuples.py
@@ -0,0 +1,14 @@
+# Niezmienna struktura danych
+coordinats = (4, 5)
+print(coordinats)
+print(coordinats[0])
+print(coordinats[1])
+
+# Tupples składające sie z list
+locations = [(10, 20), (30, 40), (50, 60), 10]
+
+print(locations)
+print(locations[0])
+print(locations[0][0])
+print(locations[0][1])
+print(locations[3])
---
+++ exercises/01_basics/tuple_coordinates.py
@@ -0,0 +1,14 @@
+warehouse_location = (50.72, 23.25)
+delivery_points = [(50.70, 23.20), (50.75, 23.30), (50.80, 23.35)]
+
+print("Warehouse location:")
+print(warehouse_location)
+
+print("Warehouse latitude:")
+print(warehouse_location[0])
+
+print("Warehouse longitude:")
+print(warehouse_location[1])
+
+print("First delivery point:")
+print(delivery_points[0])
