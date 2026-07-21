2c322124a5eee83cb695312e2498dbfc04b2eb66
Grzegorz <gpoznanski98@gmail.com>
2026-07-21 23:41:16 +0200

    Dictionaries basic

--- 01_basics/17_dictionaries.py
+++ 01_basics/17_dictionaries.py
@@ -1,1 +1,22 @@
-# słowniki - dictionarie
+month_conversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+# Wyswietl zawartosc z lklucza Jan
+print(month_conversions["Jan"])
+# metoda get do wyswietlania wartosci klucza
+print(month_conversions.get("Feb"))
+# metoda get do wyswietlania wartosci klucza , nie ma luv wiec jest none
+print(month_conversions.get("Luv"))
+# metoda get zw ratwoscia wskazana jak nie znajdzie klucza
+print(month_conversions.get("Luv", "Not a valid key"))
