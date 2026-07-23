df02757c9aec8caf2036ca76dd25256814325944
Grzegorz <gpoznanski98@gmail.com>
2026-07-23 22:07:40 +0200

    For loop

---
+++ 01_basics/20_for_loop.py
@@ -0,0 +1,20 @@
+# Petla for
+
+# wypisanie kolejnych znakow w slowie
+for letter in "Python":
+    print(letter)
+
+print("\n")
+
+# wypisanie kolejnych elementow listy
+friends = ["Tom", "Jimi", "Ann"]
+for friend in friends:
+    print(friend)
+
+# Wypis indeksy pierwsze 10 ale zaczynamy od 0 wiec bez 10
+for index in range(10):
+    print(index)
+
+# Wypis indexy od 3 do 10 ale bez 10 tu bedzie
+for index in range(3, 10):
+    print(index)
---
+++ exercises/01_basics/20_for_loop_topics.py
@@ -0,0 +1,11 @@
+# cwiczenie petli for
+topics = ["Python", "SQL", "Git", "AWS", "Docker"]
+
+# Wypisanie elementow z listy
+for topic in topics:
+    print("I am learning " + topic)
+
+
+# Wypisanie liczby do 6 a wiec 1,2,3,4,5 bez 6
+for number in range(1, 6):
+    print(number)
